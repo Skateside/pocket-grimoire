5ffccf7542d0fe102c9ab518fb7fea10c11aa295
Skateside <sk85ide@hotmail.com>
2024-04-20 13:41:59 +0100

    Update the night order class
    Rather than removing and redrawing all elements in the night order lists
    each time a change is made, this update swaps out a comment for the
    element (or vice versa) so that the lists can be updated by only
    updating the necessary elements.
    
    This commit also sets it so that showing all characters not in play will
    make sure that the dead characters are also being shown since this seems
    more intuative.
    
    See: #100

--- assets/js/classes/NightOrder.js
+++ assets/js/classes/NightOrder.js
@@ -1,57 +1,114 @@
+import CharacterToken from "./CharacterToken";
+
+/**
+ * A class that manages the night order lists.
+ */
 export default class NightOrder {
 
     constructor() {
 
-        this.showNotInPlay = false;
+        /**
+         * Whether or not to show the dead characters.
+         * @type {Boolean}
+         */
         this.showDead = false;
+
+        /**
+         * Whether or not to show the characters that are current not in play.
+         * @type {Boolean}
+         */
+        this.showNotInPlay = false;
+
+        /**
+         * The data that will display the night order lists.
+         * @type {Array.<Object>}
+         */
         this.data = [];
 
-        this.reset();
-
     }
 
+    /**
+     * Resets the lists so that the elements have been removed and the data
+     * cleared.
+     */
     reset() {
 
-        this.data.length = 0;
-        this.redraw();
+        const {
+            data,
+        } = this;
+
+        data.forEach(({ first, other }) => {
+
+            if (first) {
+
+                first.placeholder.remove();
+                first.element.remove();
+
+            }
+
+            if (other) {
+
+                other.placeholder.remove();
+                other.element.remove();
+
+            }
+
+        });
+
+        data.length = 0;
 
     }
 
-    redraw() {
-        return;
-    }
-
-    setShowNotInPlay(showNotInPlay) {
-
-        this.showNotInPlay = showNotInPlay;
-        this.redraw();
-
-    }
-
+    /**
+     * Updates {@link NightOrder#showDead}.
+     *
+     * @param {Boolean} showDead
+     *        true to show the dead characters, false to hide them.
+     */
     setShowDead(showDead) {
 
         this.showDead = showDead;
-        this.redraw();
+        this.drawAllCharacters();
 
     }
 
+    /**
+     * Updates {@link NightOrder#showNotInPlay}.
+     *
+     * @param {Boolean} showNotInPlay
+     *        true to show the not-in-play characters, false to hide them.
+     */
+    setShowNotInPlay(showNotInPlay) {
+
+        this.showNotInPlay = showNotInPlay;
+        this.drawAllCharacters();
+
+    }
+
+    /**
+     * Sets the characters that are in the lists.
+     *
+     * @param {Array.<CharacterToken>} characters
+     */
     setCharacters(characters) {
 
         characters.forEach((character) => {
 
             const data = {
                 character,
+                alive: 0,
                 inPlay: 0,
-                alive: 0
             };
             const firstNight = character.getFirstNight();
             const otherNight = character.getOtherNight();
+            const id = character.getId();
 
             if (firstNight) {
 
                 data.first = {
                     order: firstNight,
-                    element: character.drawNightOrder(true).firstElementChild
+                    placeholder: document.createComment(id),
+                    element: character.drawNightOrder(true).firstElementChild,
                 };
 
             }
@@ -60,7 +117,8 @@ export default class NightOrder {
 
                 data.other = {
                     order: otherNight,
-                    element: character.drawNightOrder(false).firstElementChild
+                    placeholder: document.createComment(id),
+                    element: character.drawNightOrder(false).firstElementChild,
                 };
 
             }
@@ -69,14 +127,21 @@ export default class NightOrder {
 
         });
 
-        this.redraw();
-
     }
 
+    /**
+     * Draws the night order list for either the first night or the other
+     * nights.
+     *
+     * @param  {Boolean} isFirstNight
+     *         true to draw the first night, false to draw the other nights.
+     * @return {DocumentFragment}
+     *         A document fragment with the night order list.
+     */
     drawNightOrder(isFirstNight) {
 
         const {
-            data
+            data,
         } = this;
         const property = (
             isFirstNight
@@ -86,18 +151,19 @@ export default class NightOrder {
 
         return data
             .filter((data) => data[property])
-            .filter((data) => this.shouldShow(data))
             .sort((a, b) => a[property].order - b[property].order)
             .reduce((fragment, data) => {
 
                 const {
-                    element
+                    element,
+                    placeholder,
                 } = data[property];
 
-                element.classList.toggle("is-in-play", this.isInPlay(data));
-                element.classList.toggle("is-dead", this.isDead(data));
-
-                fragment.append(element);
+                fragment.append(
+                    this.shouldShow(data)
+                    ? element
+                    : placeholder
+                );
 
                 return fragment;
 
@@ -106,22 +172,103 @@ export default class NightOrder {
 
     }
 
-    isInPlay(data) {
-        return data.inPlay > 0;
+    /**
+     * Helper function for drawing or updating a character's entry in the night
+     * order lists based on the character itself.
+     *
+     * @param {CharacterToken} character
+     *        Character whose entry should be drawn or updated.
+     */
+    drawCharacter(character) {
+        this.drawCharacterByData(this.getData(character));
     }
 
+    /**
+     * Draws or updates the entry in the night order lists for the character
+     * with the given data.
+     *
+     * @param {Object} data
+     *        Data whose character should have their night order lists updated.
+     */
+    drawCharacterByData(data) {
+
+        const shouldShow = this.shouldShow(data);
+
+        ["first", "other"].forEach((property) => {
+
+            if (!data[property]) {
+                return;
+            }
+
+            const {
+                placeholder,
+                element,
+            } = data[property];
+
+            element.classList.toggle("is-in-play", this.isInPlay(data));
+            element.classList.toggle("is-dead", this.isDead(data));
+
+            if (shouldShow && placeholder.parentElement) {
+                placeholder.replaceWith(element);
+            } else if (!shouldShow && element.parentElement) {
+                element.replaceWith(placeholder);
+            }
+
+        });
+
+    }
+
+    /**
+     * A helper function for drawing or updating all entries in the night order
+     * lists.
+     */
+    drawAllCharacters() {
+        this.data.forEach((data) => this.drawCharacterByData(data));
+    }
+
+    /**
+     * Checks to see if the given data represents a character that's fully dead
+     * (i.e. there are no character tokens for this character that are still
+     * alive).
+     *
+     * @param  {Object} data
+     *         Data to check.
+     * @return {Boolean}
+     *         true if the character is fully dead, false otherwise.
+     */
     isDead(data) {
         return data.inPlay > 0 && data.alive === 0;
     }
 
+    /**
+     * Checks to see if the given data has any characters in play.
+     *
+     * @param  {Object} data
+     *         Data to check.
+     * @return {Boolean}
+     *         true if any characters are in play, false otherwise.
+     */
+    isInPlay(data) {
+        return data.inPlay > 0;
+    }
+
+    /**
+     * Checks to see if the given data suggests that the character's entry in
+     * the night order lists should be showing.
+     *
+     * @param  {Object} data
+     *         Data to check.
+     * @return {Boolean}
+     *         true if the entries should be showing, false otherwise.
+     */
     shouldShow(data) {
 
         const {
+            showDead,
             showNotInPlay,
-            showDead
         } = this;
-        const isInPlay = this.isInPlay(data);
         const isDead = this.isDead(data);
+        const isInPlay = this.isInPlay(data);
 
         return (
             (isInPlay && !isDead)
@@ -131,6 +278,17 @@ export default class NightOrder {
 
     }
 
+    /**
+     * Gets the index in {@link NightOrder#data} for the data that represents
+     * the given character. If the character cannot be found an error is thrown.
+     *
+     * @param  {CharacterToken} character
+     *         Character whose data's index should be returned.
+     * @return {Number}
+     *         Index of the character within the data.
+     * @throws {ReferenceError}
+     *         The character must exist within the data.
+     */
     getDataIndex(character) {
 
         const id = character.getId();
@@ -150,51 +308,109 @@ export default class NightOrder {
 
     }
 
+    /**
+     * Helper function for getting the item within {@link NightOrder#data} that
+     * represents the given character.
+     *
+     * @param  {CharacterToken} character
+     *         Character whose data should be returned.
+     * @return {Object}
+     *         Data for the given character.
+     */
     getData(character) {
         return this.data[this.getDataIndex(character)];
     }
 
+    /**
+     * Updates {@link NightOrder#data} for the given character to register that
+     * it has been added in play.
+     *
+     * @param {CharacterToken} character
+     *        Character whose data should be updated.
+     */
     addCharacter(character) {
 
         const data = this.getData(character);
 
-        data.inPlay += 1;
-        data.alive = Math.min(data.alive + 1, data.inPlay);
+        this.adjustInPlay(data, 1);
 
-        this.redraw();
-
-    }
-
-    removeCharacter(character) {
-
-        const index = this.getDataIndex(character);
-        const {
-            data
-        } = this;
-
-        data.inPlay -= 1;
-
-        if (data.inPlay <= 0) {
-
-            data.splice(index, 1);
-            this.redraw();
-
+        if (!character.getIsDead()) {
+            this.adjustAlive(data, 1);
         }
 
+        this.drawCharacterByData(data);
+
     }
 
+    /**
+     * Updates {@link NightOrder#data} for the given character to register that
+     * it has been removed from play.
+     *
+     * @param {CharacterToken} character
+     *        Character whose data should be updated.
+     */
+    removeCharacter(character) {
+
+        const data = this.getData(character);
+
+        if (!character.getIsDead()) {
+            this.adjustAlive(data, -1);
+        }
+
+        this.adjustInPlay(data, -1);
+        this.drawCharacterByData(data);
+
+    }
+
+    /**
+     * Updates {@link NightOrder#data} for the given character to register that
+     * the character has either died or been revived.
+     *
+     * @param {CharacterToken} character
+     *        Character whose data should be updated.
+     * @param {Boolean} isDead
+     *        true if the character is dead, false otherwise.
+     */
     toggleDead(character, isDead) {
 
         const data = this.getData(character);
 
-        if (isDead) {
-            data.alive = Math.max(data.alive - 1, 0);
-        } else {
-            data.alive = Math.min(data.alive + 1, data.inPlay);
-        }
+        this.adjustAlive(data, (
+            isDead
+            ? -1
+            : 1
+        ));
+        this.drawCharacterByData(data);
 
-        this.redraw();
+    }
 
+    /**
+     * Adjusts the data's "alive" value based on the quantity given. The final
+     * result is limited so that it cannot exceed the "inPlay" value and cannot
+     * drop below 0.
+     *
+     * @param {Object} data
+     *        Data to update.
+     * @param {Number} quantity
+     *        Amount to update the data by. If this value is negative then the
+     *        value will be reduced.
+     */
+    adjustAlive(data, quantity) {
+        data.alive = Math.min(data.inPlay, Math.max(data.alive + quantity, 0));
+    }
+
+    /**
+     * Adjusts the data's "inPlay" value based on the quantity given. The final
+     * result is limited so that it cannot drop below 0.
+     *
+     * @param {Object} data
+     *        Data to update.
+     * @param {Number} quantity
+     *        Amount to update the data by. If this value is negative then the
+     *        value will be reduced.
+     */
+    adjustInPlay(data, quantity) {
+        data.inPlay = Math.max(0, data.inPlay + quantity);
     }
 
 }
--- assets/js/processes/night-order.js
+++ assets/js/processes/night-order.js
@@ -4,22 +4,25 @@ import {
     empty,
     lookupOne,
     lookupOneCached,
+    announceInput,
 } from "../utils/elements.js";
 
 const gameObserver = Observer.create("game");
 const tokenObserver = Observer.create("token");
 const nightOrder = new NightOrder();
-nightOrder.redraw = () => {
+// nightOrder.redraw = () => {
 
-    empty(lookupOneCached("#first-night")).append(nightOrder.drawNightOrder(true));
-    empty(lookupOneCached("#other-nights")).append(nightOrder.drawNightOrder(false));
+//     empty(lookupOneCached("#first-night")).append(nightOrder.drawNightOrder(true));
+//     empty(lookupOneCached("#other-nights")).append(nightOrder.drawNightOrder(false));
 
-};
+// };
 
 gameObserver.on("characters-selected", ({ detail }) => {
 
     nightOrder.reset();
     nightOrder.setCharacters(detail.characters);
+    empty(lookupOneCached("#first-night")).append(nightOrder.drawNightOrder(true));
+    empty(lookupOneCached("#other-nights")).append(nightOrder.drawNightOrder(false));
 
 });
 
@@ -35,6 +38,19 @@ tokenObserver.on("shroud-toggle", ({ detail }) => {
     nightOrder.toggleDead(detail.character, detail.isDead);
 });
 
+const showDead = lookupOne("#show-dead");
+
+showDead.addEventListener("change", ({ target }) => {
+
+    const showDead = target.checked;
+
+    nightOrder.setShowDead(showDead);
+    gameObserver.trigger("night-order-show-dead", {
+        showDead
+    });
+
+});
+
 lookupOne("#show-all").addEventListener("change", ({ target }) => {
 
     const showAll = target.checked;
@@ -44,15 +60,14 @@ lookupOne("#show-all").addEventListener("change", ({ target }) => {
         showAll
     });
 
-});
+    // Showing all characters not in play but hiding the dead can seem
+    // confusing. This forces "show dead" to be true when showing all, although
+    // the user can hide the dead seperately.
+    if (showAll && !showDead.checked) {
 
-lookupOne("#show-dead").addEventListener("change", ({ target }) => {
+        showDead.checked = true;
+        announceInput(showDead);
 
-    const showDead = target.checked;
-
-    nightOrder.setShowDead(showDead);
-    gameObserver.trigger("night-order-show-dead", {
-        showDead
-    });
+    }
 
 });
